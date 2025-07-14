0c5e6fa4bc9431080a02adc18afbfaab87e253e0
Adriana Paiva <itsadrianapaiva@gmail.com>
2025-07-14 21:16:08 +0000

    updated docker files

--- src/config/env.js
+++ src/config/env.js
@@ -1,20 +1,25 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config(); //Load .env into process.env
+// Load .env file only if not in Gitpod
+if (!process.env.GITPOD_WORKSPACE_ID) {
+  dotenv.config();
+}
 
 export const env = {
-    port: process.env.PORT || 3000,
-    db: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    },
-    redis: {
-        host: process.env.REDIS_HOST
-    },
-    jwt: {
-        secret: process.env.JWT_SECRET,
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    }
-}
+  port: process.env.PORT || 3000,
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+};
