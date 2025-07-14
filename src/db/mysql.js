d69eb5f21ce257a8d0fb8f1b65c3fd35e44d6771
Adriana Paiva <adrianapaivausa@icloud.com>
2025-07-14 18:32:30 +0100

    auth service added

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,1 +1,12 @@
-//connection pool utility
+import mysql from "mysql2/promise";
+import { env } from "../config/env.js";
+
+export const pool = mysql.createPool({
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, //good for small to medium apps
+  queueLimit: 10, //prevent inifinite queueing while handling traffic spikes
+});
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,1 +1,31 @@
-//isolated user logic
+import bcrypt from "bcrypt";
+import { pool } from "../db/mysql.js";
+
+export async function createUser({ name, email, password, role }) {
+  const passwordHash = await bcrypt.hash(password, 12);
+
+  // SQL Insert Query
+  const query = `INSERT INTO users (name, email, passwordHash, role)
+    VALUES (?, ?, ?, ?)
+    `;
+
+  try {
+    const [result] = await pool.execute(query, [
+      name,
+      email,
+      passwordHash,
+      role,
+    ]);
+    return {
+      id: result.insertId,
+      name,
+      email,
+      role,
+    };
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      throw new Error("Email already exists");
+    }
+    throw error;
+  }
+}
